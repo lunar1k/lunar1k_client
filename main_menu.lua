a10d6caee7e83a55457ab523d8139ece0f550ee2
lunar1k <dimkadiscord830@gmail.com>
2025-07-12 12:29:32 +0300

    Create main_menu.lua

---
+++ main_menu.lua
@@ -0,0 +1,22 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Lunar1k Client",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Status: Loading",
+   LoadingSubtitle = "by treecolor",
+   Theme = "Dark Blue", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+local Tab = Window:CreateTab("AimBot", 4483362458)
+local Tab2 = Window:CreateTab("Visual", 4483362458)
+local Tab3 = Window:CreateTab("PLayer", 4483362458)
+local Tab4 = Window:CreateTab("Gun", 4483362458)
+local Tab5 = Window:CreateTab("Misc", 4483362458)
+
+local Button = Tab:CreateButton({
+   Name = "Aimbot",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+   end,
+})
+    
